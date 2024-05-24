8f852f8bf937eef48864c24107016ed809056a25
Silviu Stoian <silviustoian@Silvius-MacBook-Pro.local>
2024-05-24 19:15:08 +0300

    working

---
+++ components/banner.js
@@ -0,0 +1,15 @@
+const Banner = () => {
+  return (
+    <header>
+      <div>
+        <img src="./logo.png" alt="logo" />
+      </div>
+      <div>
+        Providing houses all over the world!sfs    test
+
+      </div>
+    </header>
+  );
+};
+
+export default Banner;
