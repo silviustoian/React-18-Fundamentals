825aaef6676df32c80d80a716d165921d162461c
Silviu Stoian <silviustoian@Silvius-MacBook-Pro.local>
2024-05-27 14:49:36 +0300

    styling components

--- components/banner.js
+++ components/banner.js
@@ -1,12 +1,19 @@
+import { logo } from "./banner.module.css";
+
+const subtitleStyle = {
+  fontStyle: "italic",
+  fontSize: "x-large",
+  color: "coral",
+};
+
 const Banner = () => {
   return (
-    <header>
-      <div>
-        <img src="./logo.png" alt="logo" />
+    <header className="row mb-4">
+      <div className="col-5">
+        <img src="./logo.png" alt="logo" className={logo} />
       </div>
-      <div>
-        Providing houses all over the world!sfs    testing commit
-
+      <div className="col-7 mt-5" style={subtitleStyle}>
+        Providing houses all over the world!
       </div>
     </header>
   );
